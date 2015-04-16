1c3c742c4942fc3d7b68baeb7639d0feddf0e871
Sebastian <sbyrum@gmu.edu>
2015-04-16 14:34:07 -0400

    qertybuimo,p.;;,kjhgvtcyrxewxrctbhunjmk,l.;[

--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Not Larry");
+        Person person = new Person("Larry");
         assertEquals("Larry", person.getName());
     }
 }
